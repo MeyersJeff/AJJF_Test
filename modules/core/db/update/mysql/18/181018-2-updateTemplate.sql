a5abc3407ad2b5835346bccabfcb237ca423de10
Jeff Meyers <34994409+MeyersJeff@users.noreply.github.com>
2018-10-22 10:22:08 -0400

    Edit SQL script to end line with ; instead of ^

--- modules/core/db/update/mysql/18/181018-2-updateTemplate.sql
+++ modules/core/db/update/mysql/18/181018-2-updateTemplate.sql
@@ -1,3 +1,3 @@
-alter table AJJFTEST_TEMPLATE add column DATA_TYPE varchar(50) ^
+alter table AJJFTEST_TEMPLATE add column DATA_TYPE varchar(50) ;
 update AJJFTEST_TEMPLATE set DATA_TYPE = 'M' where DATA_TYPE is null ;
 alter table AJJFTEST_TEMPLATE modify column DATA_TYPE varchar(50) not null ;
